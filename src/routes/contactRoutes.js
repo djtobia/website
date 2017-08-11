c2e8ba5652d3e1a3ad9ba35b28be190fa92f08cd
Dylan Tobia <djtobia@gmail.com>
2017-08-11 03:17:04 -0700

    gooih

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -57,20 +57,21 @@ contactRouter.route('/sendEmail').post(function (req, res) {
 
     console.log('transporter created');
     console.log('transporter sending mail');
-    var emailSent = false;
+
     transporter.sendMail(mailOptions, function (err, info) {
-        
+
         if (err) {
             console.log(err);
+            res.send({'emailSent' : false});
         } else {
             console.log('mail sent');
-            emailSent = true;
+
         }
     });
     transporter.close();
     console.log('transporter closed');
 
-    res.send({'emailSent': emailSent});
+    res.send({'emailSent': true});
 
 })
 ;
