8d389f0968824e673fbea7c158091d9aee2e3baf
Dylan Tobia <djtobia@gmail.com>
2017-08-10 23:45:51 -0700

    Still trying

--- public/js/contact.js
+++ public/js/contact.js
@@ -3,7 +3,9 @@
  */
 var app = angular.module("contactApp", []);
 app.controller('contactController', function ($scope, contactService) {
-
+    $scope,emailSent = false;
+    $scope.errorMessage;
+    $scope.emailSentMessage;
     $scope.userInfo = {'email': null, 'content': null, 'name': null};
 
     $scope.sendEmail = function (form) {
@@ -12,21 +14,23 @@ app.controller('contactController', function ($scope, contactService) {
             return;
         }
 
-        console.log("RECAPTCHA RESPOSNSE");
-        console.log(grecaptcha.getResponse());
-
-
-
-
-        // contactService.sendEmail($scope.userInfo).then(function success(res) {
-        //         console.log(res);
-        //         console.log("email sent");
-        //     }, function error(res) {
-        //         console.log(res);
-        //         console.log("problem sending email");
-        //         $scope.errorMessage = 'There has been an error sending your email. Please try again later, or manually send an email to dylan@dylantobia.com, with the subject line "dylantobia.com contact"';
-        //     }
-        // )
+        console.log(grecaptcha.getResponse() == null);
+       if(grecaptcha.getResponse() != null){
+        contactService.sendEmail($scope.userInfo).then(function success(res) {
+                console.log(res);
+                console.log('email sent');
+                $scope.emailSent = true;
+                $scope.emailSentMessage = "Your email has been sent.";
+                $scope.errorMessage = null;
+            }, function error(res) {
+                console.log(res);
+                console.log('problem sending email');
+                $scope.errorMessage = 'There has been an error sending your email. Please try again later, or manually send an email to dylan@dylantobia.com, with the subject line "dylantobia.com contact"';
+            }
+        )}
+        else{
+           $scope.errorMessage = "You must check the recaptcha.";
+       }
     }
 
 }).service('contactService', function ($http) {
