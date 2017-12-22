26ed24a38eec56f973693fc3e451f60ad00eee30
Dylan Tobia <djtobia@gmail.com>
2017-12-22 12:19:27 -0700

    another new try

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -2,19 +2,34 @@ var express = require('express');
 var contactRouter = express.Router();
 var NodeMailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
-
+var emailjs = require('emailjs/email');
 contactRouter.route('/').get(function (req, res) {
     res.render('contact');
 });
 
 contactRouter.route('/sendEmail').post(function (req, res) {
 
-    emailjs.send("gmail","template",{reply_to: req.body.contact.email, from_name: req.body.contact.name, message: req.body.contact.content}).then(function(resposne){
-        console.log("Email Sent");
-    }, function(err){
-        console.log(err);
-        console.log("Email Failed");
+    var server = emailjs.server.connect({
+        user: "dylantobiawebsite@gmail.com",
+        password: "King's Cross 1025!",
+        host: "smtp.gmail.com"
     });
+    var message = {
+        text: req.body.contact.content,
+        from: req.body.conact.email,
+        to: "djtobia@gmail.com",
+        subject: 'CONTACT FROM ' + req.body.contact.name + ' <' + req.body.contact.email + '>' + ' DYLANTOBIA.COM'
+    };
+
+    server.send(message,function(err,message) {
+        console.log(err || message);
+    });
+    // emailjs.send("gmail","template",{reply_to: req.body.contact.email, from_name: req.body.contact.name, message: req.body.contact.content}).then(function(resposne){
+    //     console.log("Email Sent");
+    // }, function(err){
+    //     console.log(err);
+    //     console.log("Email Failed");
+    // });
     // console.log('creating transport');
     // var transporter = NodeMailer.createTransport(smtpTransport({
     //     service: 'Gmail',
