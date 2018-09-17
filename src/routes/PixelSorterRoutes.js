4db8dc3c2b6b84dbf18796cbc35d77e03af80197
Dylan Tobia <djtobia@gmail.com>
2018-09-16 18:04:12 -0700

    neat

--- src/routes/PixelSorterRoutes.js
+++ src/routes/PixelSorterRoutes.js
@@ -9,7 +9,7 @@ var fs = require('fs');
 
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, "./uploads");
+        callback(null, "public/uploads");
     },
     filename: function (req, file, callback) {
         callback(null, file.originalname);
@@ -48,7 +48,13 @@ pixelSorterRouter.route('/upload').post(function(req,res,next) {
             }
 
             console.log(req.file.originalname + " has been uploaded");
-            exec('java -jar public/jars/PixelSorter.jar uploads/' + req.file.originalname + " " +req.body.sortType,
+            var newFileName = removeSpaces(req.file.originalname);
+            fs.rename('public/uploads/'+req.file.originalname,'public/uploads/'+newFileName,function(err){
+                if(err)
+                    console.log(err);
+            });
+            console.log('java -jar public/jars/PixelSorter.jar public/uploads/' + newFileName + " " +req.body.sortType);
+            exec('java -jar public/jars/PixelSorter.jar public/uploads/' + newFileName + " " +req.body.sortType,
                 function (error, stdout, stderr) {
                     console.log("Ran Program");
                     if (error != null) {
@@ -56,31 +62,23 @@ pixelSorterRouter.route('/upload').post(function(req,res,next) {
                     }
 
                     var options = {
-                        root: "uploads/",
+                        root: "public/uploads/",
                         dotfiles: 'deny',
                         headers: {
                             'x-timestamp': Date.now(),
                             'x-sent': true
                         }
                     };
-                    var filenames = getFileNames(req.file.originalname);
+                    var filenames = getFileNames(newFileName, req.body.sortType);
 
-                    if (req.body.sortType == "gradient") {
+
                         res.sendFile(filenames[0], options, function (err) {
                             if (err) {
                                 console.log("There was a problem sending you file " + filenames[0]);
                             }
                             deleteFiles(filenames);
                         });
-                    }
-                    else {
-                        res.sendFile(filenames[1], options, function (err) {
-                            if (err) {
-                                console.log("There was a problem sending you file " + filenames[1]);
-                            }
-                            deleteFiles(filenames);
-                        })
-                    }
+                   
 
                 });
 
@@ -88,9 +86,34 @@ pixelSorterRouter.route('/upload').post(function(req,res,next) {
         });
 
 });
+
+function removeSpaces(fileName) {
+    var countSpaces = [];
+    for (var i = 0; i < fileName.length; i++) {
+        if (fileName.charAt(i) == " ") {
+            countSpaces.push(i);
+        }
+    }
+
+    var txt = "";
+    console.log("num spaces  : " + countSpaces.length);
+    if (countSpaces.length > 0) {
+
+        for (var i = 0; i < countSpaces.length; i++) {
+            txt = fileName.slice(0, countSpaces[i]) + fileName.slice(countSpaces[i] + 1);
+        }
+        console.log(txt);
+
+        return txt;
+    }
+else
+    return fileName;
+
+
+}
 function deleteFiles(filenames){
     filenames.forEach(function(name){
-        fs.unlink('uploads/'+name, function(err){
+        fs.unlink('public/uploads/'+name, function(err){
             if(err){
                 console.log(err);
             }
@@ -98,14 +121,16 @@ function deleteFiles(filenames){
     }
     );
 };
-function getFileNames(originalname){
+function getFileNames(originalname, sortType){
     var names = [];
 
     var length = originalname.length;
     var sub = originalname.substring(0,length-4);
+    if(sortType == "gradient")
+        names.push(sub+"GradientSorted.jpg");
+    else
+        names.push(sub+"Sorted.jpg");
 
-    names.push(sub+"GradientSorted.jpg");
-    names.push(sub+"Sorted.jpg");
     names.push(originalname);
     console.log(names);
     return names;
