72d554b1512415fcc9bccdbf568d0d2b64b47809
Dylan Tobia <djtobia@gmail.com>
2020-10-25 23:16:08 -0700

    Replaced contact stuff with mailto link, and updated projects.json

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 var app = express();
 var projectRouter = require('./src/routes/projectRoutes');
-var adminRouter = require('./src/routes/adminRoutes');
 var contactRouter = require('./src/routes/contactRoutes');
-var authRouter = require('./src/routes/authRoutes');
 var extrasRouter = require('./src/routes/extrasRoutes');
 var pixelSorterRouter = require('./src/routes/PixelSorterRoutes');
 var bodyParser = require('body-parser');
@@ -38,16 +36,11 @@ app.listen(port, function (err) {
 
 
 app.use('/projects', projectRouter);
-app.use('/admin', adminRouter);
 app.use('/contact', contactRouter);
-app.use('/auth', authRouter);
 app.use('/extras', extrasRouter);
 app.use('/pixelSorter', pixelSorterRouter);
 app.get('/', function (req, res) {
 
-    let images = [];
-    // mongodb.connect(url, function (err, db) {
-    //     var collection = db.collection('projects');
     fs.readFile('./projects.json', 'utf8', (err, jsonString) => {
         if (err) {
             console.log('ya fucked it dylan');
@@ -58,26 +51,13 @@ app.get('/', function (req, res) {
         let images = [];
         for (const [index, project] of Object.entries(jsonProjects)) {
             projects.push(project);
-            if(images.length < 3){
+            if (images.length < 3) {
                 images.push(project.fileName ? `<img class="projectImage" src="/images/${project.fileName}" />` : '<div class="imageBuffer"></div>');
             }
         }
         res.render('index', {projects: projects, images: images});
     });
-
-
-
-
-        // } (err, results) {
-        //     var images = [results[0].fileName ? '<img class="projectImage" src="/images/' + results[0].fileName + '" />' : '<div class="imageBuffer"></div>',
-        //         results[1].fileName ? '<img class="projectImage" src="/images/' + results[1].fileName + '" />' : '<div class="imageBuffer"></div>',
-        //         results[2].fileName ? '<img class="projectImage" src="/images/' + results[2].fileName + '" />' : '<div class="imageBuffer"></div>'];
-        //
-        //
-        //     res.render('index', {projects: results, images: images});
-        // });
-    });
-// });
+});
 
 
 app.get('/accessDenied', function (req, res) {
@@ -93,4 +73,3 @@ app.get('*', function (req, res) {
     res.render('error');
 });
 
-//module.exports = child;
--- src/routes/PixelSorterRoutes.js
+++ src/routes/PixelSorterRoutes.js
@@ -20,70 +20,63 @@ var upload = multer({
     storage: storage
 }).single('image');
 
-//var child = exec('java -jar D:/Dylan/Documents/PixelSorter/out/artifacts/PixelSorter_jar/PixelSorter.jar C:/Users/Dylan/Desktop/excellent.bmp',
-//     function(error,stdout,stderr){
-//         console.log("Ran program");
-//         if(error != null){
-//             console.log("THere was an error " + error);
-//         }
-// });
 pixelSorterRouter.route('/')
     .get(function (req, res) {
-            res.render('pixelSorter');
+        res.render('pixelSorter');
 
     });
 
-pixelSorterRouter.route('/upload').post(function(req,res,next) {
+pixelSorterRouter.route('/upload').post(function (req, res, next) {
 
-        console.log("posted to /upload");
-        upload(req, res, function (err) {
-            console.log(req.file);
-            if(req.file == null){
-                res.render("mustUploadFile");
-                return;
-            }
+    console.log("posted to /upload");
+    upload(req, res, function (err) {
+        console.log(req.file);
+        if (req.file == null) {
+            res.render("mustUploadFile");
+            return;
+        }
 
-            if (err) {
-                return res.end("There was a problem uploading your file");
-            }
+        if (err) {
+            return res.end("There was a problem uploading your file");
+        }
 
-            console.log(req.file.originalname + " has been uploaded");
-            var newFileName = removeSpaces(req.file.originalname);
-            fs.rename('public/uploads/'+req.file.originalname,'public/uploads/'+newFileName,function(err){
-                if(err)
-                    console.log(err);
-            });
-            console.log('java -jar public/jars/PixelSorter.jar public/uploads/' + newFileName + " " +req.body.sortType);
-            exec('java -jar public/jars/PixelSorter.jar public/uploads/' + newFileName + " " +req.body.sortType,
-                function (error, stdout, stderr) {
-                    console.log("Ran Program");
-                    if (error != null) {
-                        console.log(error);
+        console.log(req.file.originalname + " has been uploaded");
+        var newFileName = removeSpaces(req.file.originalname);
+        fs.rename('public/uploads/' + req.file.originalname, 'public/uploads/' + newFileName, function (err) {
+            if (err)
+                console.log(err);
+        });
+        console.log('java -jar public/jars/PixelSorter.jar public/uploads/' + newFileName + " " + req.body.sortType);
+        exec('java -jar public/jars/PixelSorter.jar public/uploads/' + newFileName + " " + req.body.sortType,
+            function (error, stdout, stderr) {
+                console.log("Ran Program");
+                if (error != null) {
+                    console.log(error);
+                }
+
+                var options = {
+                    root: "public/uploads/",
+                    dotfiles: 'deny',
+                    headers: {
+                        'x-timestamp': Date.now(),
+                        'x-sent': true
                     }
-
-                    var options = {
-                        root: "public/uploads/",
-                        dotfiles: 'deny',
-                        headers: {
-                            'x-timestamp': Date.now(),
-                            'x-sent': true
-                        }
-                    };
-                    var filenames = getFileNames(newFileName, req.body.sortType);
+                };
+                var filenames = getFileNames(newFileName, req.body.sortType);
 
 
-                        res.sendFile(filenames[0], options, function (err) {
-                            if (err) {
-                                console.log("There was a problem sending you file " + filenames[0]);
-                            }
-                            deleteFiles(filenames);
-                        });
-
-
+                res.sendFile(filenames[0], options, function (err) {
+                    if (err) {
+                        console.log("There was a problem sending you file " + filenames[0]);
+                    }
+                    deleteFiles(filenames);
                 });
 
 
-        });
+            });
+
+
+    });
 
 });
 
@@ -96,51 +89,41 @@ function removeSpaces(fileName) {
     }
 
     var txt = "";
-    console.log("num spaces  : " + countSpaces.length);
     if (countSpaces.length > 0) {
 
         for (var i = 0; i < countSpaces.length; i++) {
             txt = fileName.slice(0, countSpaces[i]) + fileName.slice(countSpaces[i] + 1);
         }
-        console.log(txt);
 
         return txt;
-    }
-else
-    return fileName;
-
-
+    } else
+        return fileName;
 }
-function deleteFiles(filenames){
-    filenames.forEach(function(name){
-        fs.unlink('public/uploads/'+name, function(err){
-            if(err){
-                console.log(err);
-            }
-        })
-    }
+
+function deleteFiles(filenames) {
+    filenames.forEach(function (name) {
+            fs.unlink('public/uploads/' + name, function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            })
+        }
     );
 };
-function getFileNames(originalname, sortType){
+
+function getFileNames(originalname, sortType) {
     var names = [];
 
     var length = originalname.length;
-    var sub = originalname.substring(0,length-4);
-    if(sortType == "gradient")
-        names.push(sub+"GradientSorted.jpg");
+    var sub = originalname.substring(0, length - 4);
+    if (sortType == "gradient")
+        names.push(sub + "GradientSorted.jpg");
     else
-        names.push(sub+"Sorted.jpg");
+        names.push(sub + "Sorted.jpg");
 
     names.push(originalname);
-    console.log(names);
     return names;
 }
-
-
-
-
-
-
 module.exports = pixelSorterRouter;
 
 
